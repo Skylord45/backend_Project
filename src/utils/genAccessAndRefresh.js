e923b141038a8633e6d226db9abd174f1d10bed7
Hemal Bhatti <bhattihemal004@gmail.com>
2024-04-06 00:56:36 +0530

    Generating accessToken and refreshToken method

---
+++ src/utils/genAccessAndRefresh.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model"
+import { ApiError } from "./ApiError"
+
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+
+       const user = await User.findById(userId)
+       const accessToken = user.generateAccessToken()
+       const refreshToken = user.generateRefreshToken()
+
+       user.refreshToken = refreshToken;
+
+       await user.save({ validateBeforeSave : false})
+
+       return {accessToken, refreshToken}
+
+        
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating refresh and access token")
+    }
+}
+
+export {generateAccessTokenAndRefreshToken}
