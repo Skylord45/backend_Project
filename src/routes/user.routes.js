b1c87f6a4c68803a80f970169bef768b235bdea6
Hemal Bhatti <bhattihemal004@gmail.com>
2024-04-20 10:48:34 +0530

    add route for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken, changeUserPassword, getCurrentUser} from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeUserPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +26,15 @@ router.route("/login").post(loginUser)
 // secured route (mean user must be login..)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-AccessToken").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeUserPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// param mathi data aave tyare aa rite lakhva "/channel/:user---" 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile),
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
